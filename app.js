279c0c0dd22928d9096600bb3245d3b7fa27a939
baraklrr <72610562+baraklrr@users.noreply.github.com>
2022-03-28 23:11:22 +0300

    Update app.js
    changed port to 3000

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = 3000
 
 const indexRouter = require('./routes')
 
