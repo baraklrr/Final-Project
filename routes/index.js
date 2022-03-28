20560944aa193d05a1bf9df12833682c99be5da6
baraklrr <72610562+baraklrr@users.noreply.github.com>
2022-03-28 23:25:40 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res) => {
-        res.send('Hello rout')
+        res.send('Hello barak')
 })
 
 module.exports = router
