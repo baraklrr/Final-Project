13cfdcea8baaa4fb690e130132f4489202957cfd
Bellavely <90250915+Bellavely@users.noreply.github.com>
2022-04-04 19:03:44 +0300

    login screens

--- Xcount/App.js
+++ Xcount/App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Provider } from 'react-native-paper'
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { theme } from './core/theme'
+import {
+  LoginScreen,
+  RegisterScreen,
+  Dashboard,
+} from './screens'
+
+
+const Stack = createStackNavigator()
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Xcount app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <Provider theme={theme}>
+  <NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="LoginScreen"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      <Stack.Screen name="Dashboard" component={Dashboard} />
+    </Stack.Navigator>
+  </NavigationContainer>
+</Provider>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
