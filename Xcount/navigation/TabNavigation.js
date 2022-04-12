59a0a3b29745ff845b9d9f053f98917f4ef5b019
Bellavely <90250915+Bellavely@users.noreply.github.com>
2022-04-13 00:33:58 +0300

    update tabnavigation

--- Xcount/navigation/TabNavigation.js
+++ Xcount/navigation/TabNavigation.js
@@ -1,15 +1,47 @@
 import React from 'react'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Dashboard from '../screens/Dashboard';
 import SettingsScreen from '../screens/SettingsScreen';
-
+import NotificationScreen from '../screens/NotificationScreen';
 const Tab = createBottomTabNavigator();
+
 
 const TabNavigation = () =>{
 return (
-<Tab.Navigator>
-<Tab.Screen name="Home" component={Dashboard}  options={{headerShown:true,headerBackVisble:false,headerTitleAlign:'center'}}/>
-<Tab.Screen name="SettingsScreen" component={SettingsScreen}/>
+<Tab.Navigator screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+}}>
+
+<Tab.Screen name="בית" component={Dashboard}  options={
+    {headerShown:true,
+    headerBackVisble:false,
+    headerTitleAlign:'center', tabBarIcon: ({color, size}) => (
+    <Ionicons name="home-outline" color={color} size={size} />)}}/>
+
+
+<Tab.Screen name="העסק שלי"  component={NotificationScreen}
+options=
+{{tabBarIcon: ({color, size}) => (
+    <Ionicons name="person" color={color} size={size} />)}}/>   
+
+
+<Tab.Screen name="הוספה" component={Dashboard} options=
+{{tabBarIcon: ({color, size}) => (
+    <Ionicons name="add" color={color} size={size} />)}}/>
+
+<Tab.Screen name="התראות"  component={NotificationScreen}
+options=
+{{tabBarIcon: ({color, size}) => (
+    <Ionicons name="alert" color={color} size={size} />)}}/>   
+
+<Tab.Screen name="הגדרות" component={SettingsScreen}
+options=
+{{tabBarIcon: ({color, size}) => (
+    <Ionicons name="settings" color={color} size={size} />)}}/>
+
+
 </Tab.Navigator>
  );
 };
