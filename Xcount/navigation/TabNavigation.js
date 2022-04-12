8c3d650462fcc663c3fc309afc5bd0e034cad352
shiran <shiranp1234@gmail.com>
2022-04-13 01:37:40 +0300

    add profile screen; add tab icons;

--- Xcount/navigation/TabNavigation.js
+++ Xcount/navigation/TabNavigation.js
@@ -1,17 +1,58 @@
-import React from 'react'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Dashboard from '../screens/Dashboard';
-import SettingsScreen from '../screens/SettingsScreen';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Dashboard from "../screens/Dashboard";
+import SettingsScreen from "../screens/SettingsScreen";
+import MyProfileScreen from "../screens/MyProfileScreen";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Tab = createBottomTabNavigator();
 
-const TabNavigation = () =>{
-return (
-<Tab.Navigator>
-<Tab.Screen name="Home" component={Dashboard}  options={{headerShown:true,headerBackVisble:false,headerTitleAlign:'center'}}/>
-<Tab.Screen name="SettingsScreen" component={SettingsScreen}/>
-</Tab.Navigator>
- );
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Dashboard}
+        options={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="cog-outline"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="MyProfile"
+        component={MyProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="account-circle"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 };
 
 export default TabNavigation;
