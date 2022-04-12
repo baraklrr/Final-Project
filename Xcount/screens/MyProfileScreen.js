3f9a73ea222548d4aefa57c83294b8a2fc766cc1
shiran <shiranp1234@gmail.com>
2022-04-13 01:39:01 +0300

    implement my-profile screen page

---
+++ Xcount/screens/MyProfileScreen.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { StyleSheet, View, Image } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function MyProfileScreen(props) {
+  return (
+    <SafeAreaProvider style={styles.container}>
+      {/* <View style={styles.header}></View> */}
+      {/*<TopBarNavigator />*/}
+      <Image
+        style={styles.avatar}
+        source={{ uri: "https://bootdey.com/img/Content/avatar/avatar6.png" }}
+      />
+      {/* <MyTabs/> */}
+    </SafeAreaProvider>
+  );
+}
+
+/*<NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen name="Home" component={HomeScreen} />
+                    <Tab.Screen name="Settings" component={SettingsScreen} />
+                </Tab.Navigator>
+              </NavigationContainer>*/
+
+const styles = StyleSheet.create({
+  displayUser: {},
+  existButton: {},
+  header: {
+    backgroundColor: "#00BFFF",
+    height: 70,
+    display: "flex",
+    flexDirection: "column",
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom: 10,
+    alignSelf: "center",
+    position: "absolute",
+    marginTop: 35,
+  },
+  name: {
+    fontSize: 22,
+    color: "#FFFFFF",
+    fontWeight: "600",
+  },
+  body: {
+    marginTop: 40,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: "center",
+    padding: 30,
+  },
+  name: {
+    fontSize: 28,
+    color: "#696969",
+    fontWeight: "600",
+  },
+  info: {
+    fontSize: 16,
+    color: "#00BFFF",
+    marginTop: 10,
+  },
+  description: {
+    fontSize: 16,
+    color: "#696969",
+    marginTop: 10,
+    textAlign: "center",
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: "#00BFFF",
+  },
+});
