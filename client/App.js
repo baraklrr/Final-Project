c1459bec435601706d9afd4ffced6107554df093
alonbab <alon_bab@hotmail.com>
2022-04-13 17:35:32 +0300

    merge leftover

--- client/App.js
+++ client/App.js
@@ -2,27 +2,18 @@ import React from "react";
 import { Provider } from "react-native-paper";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { theme } from "./core/theme";
-import { LoginScreen, RegisterScreen, Dashboard, StartScreen } from "./screens";
+import AuthStack from "./navigation/AuthStack";
+import { I18nManager } from "react-native";
 
-const Stack = createStackNavigator();
+I18nManager.allowRTL(true);
+I18nManager.forceRTL(true);
 
 export default function App() {
   return (
     <Provider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="StartScreen"
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="StartScreen" component={StartScreen} />
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-          <Stack.Screen name="Dashboard" component={Dashboard} />
-        </Stack.Navigator>
+        <AuthStack />
       </NavigationContainer>
     </Provider>
   );
