f06e5bfb4d8cb0e265452a2a450966248112dff7
shiran <shiranp1234@gmail.com>
2022-04-25 20:56:33 +0300

    save changes

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -2,23 +2,17 @@ const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-  dbConfig.DB,
-  dbConfig.USER,
-  dbConfig.PASSWORD,
-  {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorsAliases: false,
-    pool: {
-      max: dbConfig.pool.max,
-      min: dbConfig.pool.min,
-      acquire: dbConfig.pool.acquire,
-      idle: dbConfig.pool.idle
-    }
-  }
-);
-
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
 
 // const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
 //   host: dbConfig.HOST,
@@ -40,25 +34,37 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.invoices = require("./invoice.model.js")(sequelize, Sequelize);
+db.expense = require("./expense.model.js")(sequelize, Sequelize);
+db.income = require("./income.model.js")(sequelize, Sequelize);
+db.business = require("./business.model.js")(sequelize, Sequelize);
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
-db.refreshToken = require("../models/refreshToken.model.js")(sequelize, Sequelize);
+db.refreshToken = require("../models/refreshToken.model.js")(
+  sequelize,
+  Sequelize
+);
 db.role.belongsToMany(db.user, {
   through: "user_roles",
   foreignKey: "roleId",
-  otherKey: "userId"
+  otherKey: "userId",
 });
 db.user.belongsToMany(db.role, {
   through: "user_roles",
   foreignKey: "userId",
-  otherKey: "roleId"
+  otherKey: "roleId",
 });
 db.refreshToken.belongsTo(db.user, {
-  foreignKey: 'userId', targetKey: 'id'
+  foreignKey: "userId",
+  targetKey: "id",
 });
 db.user.hasOne(db.refreshToken, {
-  foreignKey: 'userId', targetKey: 'id'
+  foreignKey: "userId",
+  targetKey: "id",
 });
+/*db.expense.hasOne(db.refreshToken, {
+  foreignKey: 'userId', targetKey: 'id'
+});*/
+
 db.ROLES = ["user", "admin", "moderator"];
 
 module.exports = db;
