fd11a197e40b7566d8bc196b16a0de2e48a3e4cf
shiran <shiranp1234@gmail.com>
2022-04-25 20:56:08 +0300

    insert income,expense,bussines tables

--- server/app/models/user.model.js
+++ server/app/models/user.model.js
@@ -1,15 +1,40 @@
+const { user } = require(".");
+
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("users", {
     username: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
     },
     email: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
     },
     password: {
-      type: Sequelize.STRING
-    }
+      type: Sequelize.STRING,
+    },
+    phoneNumber: {
+      type: Sequelize.STRING,
+    },
+    status: {
+      type: Sequelize.ENUM("Bachelor", "Married", "divorcee"),
+    },
+    numberOfChildren: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+    },
+    businessId: {
+      type: Sequelize.INTEGER,
+    },
+    // bankAccount: {
+    //   type: Sequelize.ENUM(...Object.values(BankAccount)),
+    //   defaultValue: "pending",
+    // },
   });
 
   return User;
+};
+
+const BankAccount = {
+  HAPOALIM: "hapoalim",
+  DISCOUNT: "discount",
+  LEUMI: "leumi",
 };
