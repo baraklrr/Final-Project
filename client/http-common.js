d0568a11943ace760edbe760eac46e6adda36ecf
barak_lrr <baraklrr37@gmail.com>
2022-06-05 21:53:42 +0300

    fix view ios tabs

--- client/http-common.js
+++ client/http-common.js
@@ -1,9 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
+import { Platform } from 'react-native';
 
 export default axios.create({
-  //baseURL: "http://localhost:8080/api", // for web expo
-  baseURL: 'http://10.0.2.2:8080/api', // for android emulator
+  baseURL: Platform.OS === 'ios' ? 'http://localhost:8080/api' : 'http://10.0.2.2:8080/api', // for web expo / ios
+  // baseURL: 'http://10.0.2.2:8080/api', // for android emulator
   headers: {
-    "Content-type": "application/json",
+    'Content-type': 'application/json',
   },
 });
--- client/navigation/AuthStack.js
+++ client/navigation/AuthStack.js
@@ -1,33 +1,39 @@
-import React from "react";
+import React from 'react';
 
-import { createStackNavigator } from '@react-navigation/stack'
-import LoginScreen from '../screens/User/LoginScreen'
-import RegisterScreen from '../screens/User/RegisterScreen'
-import StartScreen from '../screens/User/StartScreen'
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/User/LoginScreen';
+import RegisterScreen from '../screens/User/RegisterScreen';
+import StartScreen from '../screens/User/StartScreen';
 import MainNavigatior from './MainNavigator';
-import {TabContextProvider} from "../context/TabContext";
-import {NavigationContainer} from "@react-navigation/native";
+import { TabContextProvider } from '../context/TabContext';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
 const AuthStack = () => {
   return (
     <TabContextProvider>
-    <NavigationContainer>
-    <Stack.Navigator
-    initialRouteName="StartScreen"
-    screenOptions={{
-      headerShown: false,
-    }}>
-    <Stack.Screen name="StartScreen" component={StartScreen} options={{title:"שלום!",           
-                //animationTypeForReplace: state.isSignout ? 'pop' : 'push',
-    }} />
-    <Stack.Screen name="LoginScreen" component={LoginScreen} />
-    <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-
-  </Stack.Navigator>
-  </NavigationContainer>
-  </TabContextProvider>)
-}
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="StartScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen
+            name="StartScreen"
+            component={StartScreen}
+            options={{
+              title: 'שלום!',
+              //animationTypeForReplace: state.isSignout ? 'pop' : 'push',
+            }}
+          />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </TabContextProvider>
+  );
+};
 
 export default AuthStack;
