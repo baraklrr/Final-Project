76be365bf9be82ead723b88e4f3a313cfed5c0df
alonbab <alon_bab@hotmail.com>
2022-06-12 17:36:34 +0300

    Pdfs

--- client/navigation/MyBuissnessNavigator.js
+++ client/navigation/MyBuissnessNavigator.js
@@ -5,8 +5,9 @@ import MyBuissnessScreen from '../screens/MyBuissnessScreen';
 import MyDetails from '../screens/MyDetails';
 import Myfiles from '../screens/MyFiles';
 import { COLORS } from '../core/theme';
-import InstructionScreen from "../screens/InstructionsScreen"
+import InstructionScreen from '../screens/InstructionsScreen';
 import ReportsNavigation from './ReportsNavigation';
+import DocViewer from '../screens/files/DocViewer';
 
 const Stack = createStackNavigator();
 
@@ -27,6 +28,11 @@ const MybuissnessNavigator = () => {
       <Stack.Screen name="ערוך את הפרטים שלי" component={MyDetails} />
       <Stack.Screen name="המסמכים שלי" component={Myfiles} />
       <Stack.Screen name="נהלים" component={InstructionScreen} />
+      <Stack.Screen
+        name="מסמך"
+        component={DocViewer}
+        options={({ route }) => ({ title: route.params.name , url: route.params.url })}
+      />
     </Stack.Navigator>
   );
 };
