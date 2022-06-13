8b6ddbe7ca0a07564c6f5165f21b0ca7be88b692
shiran <shiranp1234@gmail.com>
2022-06-13 23:29:56 +0300

    validate income

--- server/config/auth.config.js
+++ server/config/auth.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   secret: "xcount-secret-key",
-  jwtExpiration: 60 * 60, // 3600 seconds = 1 hour
+  jwtExpiration: 60 * 60 * 5, // 3600 seconds = 1 hour
   jwtRefreshExpiration: 100, // 24 hours
   /* for test */
   // jwtExpiration: 60,          // 1 minute
