1e26704c61b84aab8d7b85a1541e7ae280c79817
barak_lrr <baraklrr37@gmail.com>
2022-06-13 19:25:50 +0300

    ofir fix

--- client/screens/Dashboard.js
+++ client/screens/Dashboard.js
@@ -5,8 +5,54 @@ import TabContainer from '../components/TabContainer';
 import { COLORS } from '../core/theme';
 import Constants from 'expo-constants';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import ExpenseDataService from '../services/expense.service';
 
 export default function Dashboard({ navigation }) {
+  const [sum, setSum] = useState();
+  const [sumVat, setSumVat] = useState();
+  const [sumIrs, setSumIrs] = useState();
+  const getExpenseSum = async () => {
+    ExpenseDataService.exppenseSum()
+      .then((response) => {
+        setSum(response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data); // => the response payload
+        }
+      });
+  };
+  const getExpenseSumVat = async () => {
+    ExpenseDataService.exppenseVatSum()
+      .then((response) => {
+        setSumIrs(response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data); // => the response payload
+        }
+      });
+  };
+
+  const getExpenseSumIrs = async () => {
+    ExpenseDataService.exppenseIrsSum()
+      .then((response) => {
+        setSumVat(response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data); // => the response payload
+        }
+      });
+  };
+  useEffect(() => {
+    console.log('loading Data');
+    getExpenseSum();
+    getExpenseSumVat();
+    getExpenseSumIrs();
+  }, []);
+  console.log(sumVat);
+
   return (
     <TabContainer>
       <View style={styles.screenContainer}>
@@ -78,7 +124,7 @@ export default function Dashboard({ navigation }) {
                   <Text style={styles.name}>סך הוצאות כללית</Text>
                 </View>
                 <View style={{ flex: 1 }}>
-                  <Text style={{ textAlign: 'right', fontWeight: 'bold' }}>{'\u20AA'}1500</Text>
+                  <Text style={{ textAlign: 'right', fontWeight: 'bold' }}>{'\u20AA'}</Text>
                 </View>
               </View>
             </View>
@@ -185,7 +231,6 @@ const styles = StyleSheet.create({
   name: {
     fontSize: 16,
     paddingRight: 6,
-    textAlign: 'left',
   },
   borderText: {
     marginTop: -27,
